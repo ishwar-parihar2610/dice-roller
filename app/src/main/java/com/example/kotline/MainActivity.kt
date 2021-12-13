e5cdf5b99d25a890910ef8cf7bb344e3504163de
ishwar parihar <paruharishwer26102000@gmail.com>
2021-12-13 16:41:11 +0530

    Kotline

--- app/src/main/java/com/example/kotline/MainActivity.kt
+++ app/src/main/java/com/example/kotline/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.example.kotline
 
+import android.app.ActivityManager
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.kotline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var diceImage: ImageView;
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val rollBtn = findViewById<Button>(R.id.rollBtn);
-        diceImage=findViewById<ImageView>(R.id.imageView);
-
-        rollBtn.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.rollBtn.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
         val randomNumber = (1..6).random();
+
+
         val drawableImage =
-            when(randomNumber){
-            1->R.drawable.dice_1
-            2->R.drawable.dice_2
-            3->R.drawable.dice_3
-            4->R.drawable.dice_4
-            5->R.drawable.dice_5
-            else->R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableImage);
+            when (randomNumber) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        binding.imageView.setImageResource(drawableImage);
 
 
     }
